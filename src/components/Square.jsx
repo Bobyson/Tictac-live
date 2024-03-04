88e11022afeeb703a65b57687cd396408181df72
Bobyson <acntanouba@gmail.com>
2024-03-04 13:33:27 +0530

    Initial Commit

---
+++ src/components/Square.jsx
@@ -0,0 +1,14 @@
+
+import "./square.css";
+
+function Square({value, onSquareClick}) {
+
+
+  return (
+    <button className="square" onClick={onSquareClick} >
+      {value}
+    </button>
+  );
+}
+
+export default Square;
